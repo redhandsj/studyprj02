64a92b6620f66bd413c5794f9e0b54e8754dfe88
redhandsj <redhandsj@redhandsj>
2016-08-23 11:14:55 +0900

    SpringDataのつづきをup

--- DataApp1/src/main/java/jp/tuyano/spring/data1/App.java
+++ DataApp1/src/main/java/jp/tuyano/spring/data1/App.java
@@ -13,10 +13,15 @@ public class App {
 	private static ApplicationContext context;
 
 	public static void main(String[] args) {
+		
+		// bean.xml を取り込み
 		context = new ClassPathXmlApplicationContext("classpath:/bean.xml");
 		jdbcTemplate = context.getBean(JdbcTemplate.class);
 
+		// execute によるクエリー実行
 		jdbcTemplate.execute("insert into mypersondata (name,mail,age) values('tuyano','syoda@tuyano.com',123)");
+		
+		// DB内容を取得
 		List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from mypersondata");
 		for (Map<String, Object> obj : list) {
 			System.out.println(obj);
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# studyprj02
-SpringFramework Study Project
+# studyprj02ÅF
+* ï∂ñ@ÅF <http://codechord.com/2012/01/readme-markdown/>
+
+| SpringFramework Study Project
+
+* 
