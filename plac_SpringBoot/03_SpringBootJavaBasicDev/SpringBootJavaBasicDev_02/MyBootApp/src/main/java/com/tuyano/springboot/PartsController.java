72306040ace4bc3d7de1525cb6607a31b4c7bd89
redhandsj <redhandsj@DESKTOP-D448TDI>
2017-10-24 22:09:24 +0900

    SpringBoot追加

--- plac_SpringBoot/03_SpringBootJavaBasicDev/SpringBootJavaBasicDev_02/MyBootApp/src/main/java/com/tuyano/springboot/PartsController.java
+++ plac_SpringBoot/03_SpringBootJavaBasicDev/SpringBootJavaBasicDev_02/MyBootApp/src/main/java/com/tuyano/springboot/PartsController.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping(("parts"))
 public class PartsController {
-	
+		
 	/**
 	 * チェックボックス
 	 * @param mav
@@ -23,7 +23,32 @@ public class PartsController {
 	}
 
 	/**
-	 * チェックボックス
+	 * ラジオボタン
+	 * @param mav
+	 * @return
+	 */
+	@RequestMapping(value = "/radio" , method = RequestMethod.GET)
+	public ModelAndView radioBtn(ModelAndView mav){
+		mav.setViewName("parts/radioButton");
+		mav.addObject("msg","ラジオボタンを選択してボタン押下");
+		return mav;
+	}
+
+	/**
+	 * セレクトボックス
+	 * @param mav
+	 * @return
+	 */
+	@RequestMapping(value = "/select" , method = RequestMethod.GET)
+	public ModelAndView selectBox(ModelAndView mav){
+		mav.setViewName("parts/select");
+		mav.addObject("msg","セレクトボックスを選択してボタン押下");
+		return mav;
+	}
+
+	
+	/**
+	 * チェックボックス結果
 	 * @param mav
 	 * @return
 	 */
@@ -33,9 +58,52 @@ public class PartsController {
 		try{
 			res = "checkbox : " + chk1;
 		}catch(NullPointerException e){
-			
+			e.printStackTrace();			
 		}
 		mav.setViewName("parts/chkbox");
+		mav.addObject("msg",res);
+		return mav;
+	}
+
+	/**
+	 * ラジオボタン結果
+	 * @param mav
+	 * @return
+	 */
+	@RequestMapping(value = "/sendRadio" , method = RequestMethod.POST)
+	public ModelAndView sendRadio(@RequestParam(value="radio1", required=false)String radio1, ModelAndView mav){
+		String res="";
+		try{
+			res = "radio1 : " + radio1;
+		}catch(NullPointerException e){
+			e.printStackTrace();
+		}
+		mav.setViewName("parts/radioButton");
+		mav.addObject("msg",res);
+		return mav;
+	}
+
+	/**
+	 * ラジオボタン結果
+	 * @param mav
+	 * @return
+	 */
+	@RequestMapping(value = "/sendSelect" , method = RequestMethod.POST)
+	public ModelAndView sendSelect(
+			@RequestParam(value="sel1", required=false)String select1, 
+			@RequestParam(value="sel2", required=false)String[] select2, 
+			ModelAndView mav){
+		String res="";
+		try{
+			res = "select1 : " + select1 + "\n" +  "select2 : ";
+			for(String s : select2) {
+				res +=  s + " , ";
+			}
+		
+		}catch(NullPointerException e){
+			e.printStackTrace();
+		}
+		mav.setViewName("parts/select");
 		mav.addObject("msg",res);
 		return mav;
 	}
