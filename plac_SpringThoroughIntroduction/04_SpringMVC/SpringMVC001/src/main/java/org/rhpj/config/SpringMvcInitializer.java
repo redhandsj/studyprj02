0a8f6c39d921392a38bcef1ffba577ef83773445
redhandsj <redhandsj@redhands.com>
2017-11-14 17:55:50 +0900

    雛形追加

---
+++ plac_SpringThoroughIntroduction/04_SpringMVC/SpringMVC001/src/main/java/org/rhpj/config/SpringMvcInitializer.java
@@ -0,0 +1,32 @@
+package org.rhpj.config;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMvcInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { WebMvcConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		return new Filter[] { characterEncodingFilter };
+	}
+
+}
---
+++ plac_SpringThoroughIntroduction/04_SpringMVC/SpringMVC001/src/main/java/org/rhpj/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package org.rhpj.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("org.rhpj")
+public class WebMvcConfig  extends WebMvcConfigurerAdapter {
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp()
+				.prefix("/WEB-INF/view/")
+				.suffix(".jsp");
+	}
+
+	
+}
